b185dbf0d6ec11bf274cee1b27e75d0503e5efa7
Felix Kaiser <felix.kaiser@fxkr.net>
2017-08-19 12:43:21 +0200

    Fix test failing due to non-deterministic order of deltas

--- diff_object_test.go
+++ diff_object_test.go
@@ -2,6 +2,7 @@ package jsondelta
 
 import (
 	. "gopkg.in/check.v1"
+	"reflect"
 )
 
 type DiffObjectSuite struct {
@@ -30,13 +31,15 @@ func (s *DiffObjectSuite) TestObjectToZero(c *C) {
 func (s *DiffObjectSuite) TestObjectNewKey(c *C) {
 	left := map[string]interface{}{}
 	right := map[string]interface{}{"key1": "value1", "key2": "value2"}
-	expected := []interface{}{
-		[]interface{}{[]interface{}{"key1"}, "value1"},
-		[]interface{}{[]interface{}{"key2"}, "value2"},
-	}
-	obtained, err := diff(left, right)
+	expectedItem1 := 	[]interface{}{[]interface{}{"key1"}, "value1"}
+	expectedItem2 := []interface{}{[]interface{}{"key2"}, "value2"}
+	expected1 := []interface{}{expectedItem1, expectedItem2}
+	expected2:= []interface{}{expectedItem2, expectedItem1}
+	obtained, err := diff(left, right) // Order of patches is not stable
 	c.Assert(err, IsNil)
-	c.Assert(obtained, DeepEquals, expected)
+	equals1 := reflect.DeepEqual(obtained, expected1);
+	equals2 := reflect.DeepEqual(obtained, expected2);
+	c.Assert(equals1||equals2, Equals, true)
 }
 
 func (s *DiffObjectSuite) TestObjectRemovedKey(c *C) {
