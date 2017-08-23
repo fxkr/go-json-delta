c36dd199e7189f445ce78cf304dc712b57868060
Felix Kaiser <felix.kaiser@fxkr.net>
2017-08-23 22:54:59 +0200

    Refactoring: Extract diffArray/diffObject functions from Diff

--- diff.go
+++ diff.go
@@ -49,41 +49,7 @@ func Diff(leftStruct interface{}, rightStruct interface{}) ([]interface{}, error
 			return newDiff(newUpdateStanza(rightStruct)), nil
 		}
 
-		results := newEmptyDiff()
-
-		// Update items
-		for i := 0; i < len(leftVal) || i < len(rightVal); i++ {
-
-			// For any extra items on the left, make 'remove' stanzas
-			if i >= len(rightVal) {
-				results = append(results, newArrayRemoveStanza(i))
-				continue
-			}
-
-			// For any extra items on the right, make 'add' stanzas
-			rightListVal := rightVal[i]
-			if i >= len(leftVal) {
-				results = append(results, newArrayAddStanza(i, rightListVal))
-				continue
-			}
-
-			// Compare items at same position
-			leftListVal := leftVal[i];
-			subResults, err := Diff(leftListVal, rightListVal)
-			if err != nil {
-				return nil, errors.Wrap(err, fmt.Sprint("Error handling array item %d", i))
-			}
-
-			// For equal items at same position, do nothing
-			if len(subResults) == 0 {
-				continue
-			}
-
-			// For non-equal items, prefix their diffs stanzas with the array index and add them to the result
-			results = appendWithPrefixIndex(results, i, subResults)
-		}
-
-		return results, nil
+		return diffArray(leftVal, rightVal)
 
 	case map[string]interface{}:
 		rightVal, ok := rightStruct.(map[string]interface{})
@@ -91,48 +57,90 @@ func Diff(leftStruct interface{}, rightStruct interface{}) ([]interface{}, error
 			return newDiff(newUpdateStanza(rightStruct)), nil
 		}
 
-		results := newEmptyDiff()
-
-		// Find removed keys
-		for mapKey, _ := range leftVal {
-			_, found := rightVal[mapKey]
-			if !found {
-				results = append(results, newObjectRemoveStanza(mapKey))
-				continue
-			}
-		}
-
-		// Find added keys
-		for mapKey, rightMapVal := range rightVal {
-			_, found := leftVal[mapKey]
-			if !found {
-				results = append(results, newObjectUpdateStanza(mapKey, rightMapVal))
-				continue
-			}
-		}
-
-		// Find updated keys
-		for mapKey, leftMapVal := range leftVal {
-			rightMapVal, found := rightVal[mapKey]
-			if !found {
-				continue // Added keys already handled above
-			}
-
-			// Recursive call
-			subResults, err := Diff(leftMapVal, rightMapVal)
-			if err != nil {
-				return nil, errors.Wrap(err, fmt.Sprint("Error handling updated dict key %v", mapKey))
-			}
-
-			// Prefix sub-result keys with map key and add so modified sub-results to result
-			results = appendWithPrefixKey(results, mapKey, subResults)
-		}
-
-		return results, nil
+		return diffObject(leftVal, rightVal)
 
 	default:
 		return nil, errors.New(fmt.Sprintf("Bad type on left side: %T", leftStruct))
 	}
+}
+
+func diffArray(leftVal []interface{}, rightVal []interface{}) ([]interface{}, error) {
+	results := newEmptyDiff()
+
+	// Update items
+	for i := 0; i < len(leftVal) || i < len(rightVal); i++ {
+
+		// For any extra items on the left, make 'remove' stanzas
+		if i >= len(rightVal) {
+			results = append(results, newArrayRemoveStanza(i))
+			continue
+		}
+
+		// For any extra items on the right, make 'add' stanzas
+		rightListVal := rightVal[i]
+		if i >= len(leftVal) {
+			results = append(results, newArrayAddStanza(i, rightListVal))
+			continue
+		}
+
+		// Compare items at same position
+		leftListVal := leftVal[i];
+		subResults, err := Diff(leftListVal, rightListVal)
+		if err != nil {
+			return nil, errors.Wrap(err, fmt.Sprint("Error handling array item %d", i))
+		}
+
+		// For equal items at same position, do nothing
+		if len(subResults) == 0 {
+			continue
+		}
+
+		// For non-equal items, prefix their diffs stanzas with the array index and add them to the result
+		results = appendWithPrefixIndex(results, i, subResults)
+	}
+
+	return results, nil
+}
+
+func diffObject(leftVal map[string]interface{}, rightVal map[string]interface{}) ([]interface{}, error) {
+	results := newEmptyDiff()
+
+	// Find removed keys
+	for mapKey, _ := range leftVal {
+		_, found := rightVal[mapKey]
+		if !found {
+			results = append(results, newObjectRemoveStanza(mapKey))
+			continue
+		}
+	}
+
+	// Find added keys
+	for mapKey, rightMapVal := range rightVal {
+		_, found := leftVal[mapKey]
+		if !found {
+			results = append(results, newObjectUpdateStanza(mapKey, rightMapVal))
+			continue
+		}
+	}
+
+	// Find updated keys
+	for mapKey, leftMapVal := range leftVal {
+		rightMapVal, found := rightVal[mapKey]
+		if !found {
+			continue // Added keys already handled above
+		}
+
+		// Recursive call
+		subResults, err := Diff(leftMapVal, rightMapVal)
+		if err != nil {
+			return nil, errors.Wrap(err, fmt.Sprint("Error handling updated dict key %v", mapKey))
+		}
+
+		// Prefix sub-result keys with map key and add so modified sub-results to result
+		results = appendWithPrefixKey(results, mapKey, subResults)
+	}
+
+	return results, nil
 }
 
 func appendWithPrefixIndex(results []interface{}, index int, subResults []interface{}) []interface{} {
